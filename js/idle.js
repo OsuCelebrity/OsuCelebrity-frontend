2c2e00f6124d4e5513b3611414704c8d06fff979
Reese <metroidescapes@yahoo.com>
2016-02-07 17:12:36 -0600

    Added carousel for images on idle webpage

---
+++ js/idle.js
@@ -0,0 +1,23 @@
+$(".wallpaper-wrapper").slick({
+    autoplay: true,
+    autoplaySpeed: 2500,
+    speed: 500,
+    arrows: false,
+    fade: true,
+    infinite: true,
+    pauseOnHover: false
+});
+
+
+window.setInterval(function () {
+    $(".wallpaper-video").get(0).play();
+}, 500);
+
+
+$('.wallpaper-wrapper').on('beforeChange', function () {
+    setTimeout(
+        function () {
+            $(".wallpaper-video").get(0).currentTime = 0;
+
+        }, 0);
+});
